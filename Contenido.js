8a3ed00c51f9ad0335d41b87acc8f90a487bbd3a
Osbinceutec <osbin_salgado@unitec.edu>
2024-05-15 14:36:03 -0600

    update

--- Contenido.js
+++ Contenido.js
@@ -1,5 +1,6 @@
 function abrir_L8_A01() {
     $("#box_L8_A01").slideDown();
+    $("L8-A01_estado").slideDown();
 }
 
 function cerrar_L8_A01() {
@@ -12,7 +13,7 @@ let colorSeleccionado = ''; // Variable para almacenar el color seleccionado
             colorSeleccionado = color; // Guarda el color seleccionado
         }
 
-        function myFunction() {
+        function report_problem() {
             const div = document.getElementById('box_ext_L8_A01');
             switch (colorSeleccionado) {
                 case 'blue':
@@ -32,7 +33,24 @@ let colorSeleccionado = ''; // Variable para almacenar el color seleccionado
                     div.style.backgroundImage = 'linear-gradient(white 90%, green)'; // Color inicial si no se selecciona ningún color
             }
             alert("Tu mensaje a sido enviado correctamente");   
+
+            document.getElementById("L8-A01_estado").style.display = "block";
+        var L8_A01_estado = document.getElementById("L8-A01_estado");
+        var L8_A01_report = document.getElementById("L8-A01_report");
+
+            L8_A01_estado.style.display = "block";        
+            L8_A01_report.style.display = "none";
         }
+
+        function report() {
+            document.getElementById("L8-A01_report").style.display = "block";
+            var L8_A01_estado = document.getElementById("L8-A01_estado");
+            var L8_A01_report = document.getElementById("L8-A01_report");
+
+                L8_A01_estado.style.display = "none";        
+                L8_A01_report.style.display = "block";
+                alert("Reporta el problema que tienes con el equipo"); 
+    }
 
 
 
